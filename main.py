87b78e4a1d3b9a46ac64e724c2491227cf2c7ce8
Ilya Seleznev <zedplay.zp@gmail.com>
2024-06-12 20:07:35 +0300

    Completed 2 task

--- main.py
+++ main.py
@@ -11,6 +11,15 @@ class Rectangle(Figure):
     def area(self):
         return self.a_side * self.b_side
 
+    def __int__(self):
+        return int(self.area())
+
+    def __str__(self):
+        return (f"Прямоугольник у которого:\n"
+                f"Первая сторона  = {self.a_side}\n"
+                f"Вторая сторона = {self.b_side}")
+
+
 
 class Circle(Figure):
     def __init__(self, radius: int):
@@ -18,6 +27,13 @@ class Circle(Figure):
 
     def area(self):
         return 3.14 * self.radius ** 2
+
+    def __int__(self):
+        return int(self.area())
+
+    def __str__(self):
+        return (f"Круг у которого:\n"
+                f"Радиус  = {self.radius}")
 
 
 class RightTriangle(Figure):
@@ -28,6 +44,14 @@ class RightTriangle(Figure):
     def area(self):
         return 0.5 * self.side * self.height
 
+    def __int__(self):
+        return int(self.area())
+
+    def __str__(self):
+        return (f"Прямоугольный треугольник у которого:\n"
+                f"Основание  = {self.side}\n"
+                f"Высота = {self.height}")
+
 
 class Trapezoid(Figure):
     def __init__(self, upside: int, downside: int, height: int):
@@ -37,3 +61,12 @@ class Trapezoid(Figure):
 
     def area(self):
         return 0.5 * (self.upside + self.downside) * self.height
+
+    def __int__(self):
+        return int(self.area())
+
+    def __str__(self):
+        return (f"Трапеция у которой:\n"
+                f"Верхняя сторона = {self.upside}\n"
+                f"Нижняя сторона = {self.downside}\n"
+                f"Высота = {self.height}")
